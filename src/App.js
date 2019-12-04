078b74f56382e152aefd019b39ddfb5ba8d223da
Adam Rasheed <adamrasheed91@gmail.com>
2019-12-03 21:19:22 -0800

    added reset form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import drippLogo from './dripp-logo.svg'
 import './App.css';
 
 const App = () => {
-  const [amount, setAmount] = useState(0)
+  const initialAmount = 0
+  const [amount, setAmount] = useState(initialAmount)
 
   const handleAddAmount = ({ value = 5}) => {
    setAmount((prevAmount) => prevAmount + value)
+  }
+
+  const handleResetAmount = ({value = initialAmount}) => {
+    const msg = `Are you sure you want to reset?`
+    if (window.confirm(msg)) {
+      setAmount(initialAmount)
+    }
   }
   return (
     <div className="app">
@@ -20,7 +28,7 @@ const App = () => {
 
       <p className="total-amount">You've saved ${amount} in total.</p>
 
-      <button className="btn">Reset Amount</button>
+      <button className="btn" onClick={handleResetAmount}>Reset Amount</button>
       <p className="reset-message">
         Reset amount once you’ve put the saved amount into savings or paid off your credit card.
       </p>
