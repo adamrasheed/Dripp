7b0d43d369010fdae0ffebe3c893f78f76335891
Adam Rasheed <adamrasheed91@gmail.com>
2019-12-24 19:18:48 -0800

    added styles

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <img src={logo} alt=""/>
+      <img src={logo} alt="Dripp" className="logo"/>
       <div className="button-container">
         <button className="button button--primary">Save $5</button>
         <button className="button button--primary">Save $10</button>
+      </div>
 
-        <div className="message-container">
-          <p className="message">
-            Congrats! You've saved <span className="success-amount">$65</span> so far!
-          </p>
-        </div>
+      <div className="message-container">
+        <p className="message">
+          Congrats! You've saved <span className="message-amount">$65</span> so far!
+        </p>
+      </div>
 
-        <p className="total-count">You've saved $65 so far.</p>
+      <p className="total-count">You've saved $65 so far.</p>
 
-        <div className="reset-container">
-          <button className="button"></button>
-          <p className="small">
-            Reset amount once you’ve put the saved amount into savings or paid off your credit card.
-          </p>
-        </div>
+      <div className="reset-container">
+        <button className="button reset-button">Reset Amount</button>
+        <p className="reset-disclaimer">
+          Reset amount once you’ve put the saved amount into savings or paid off your credit card.
+        </p>
       </div>
     </div>
   );
